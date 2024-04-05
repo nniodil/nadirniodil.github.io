3abc02bf4eeb76820293183d6736cf483ec4489a
Nadir Niodil <nadirniodil@hotmail.fr>
2024-04-05 19:54:17 +0200

    added content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-site 'https://github.com/FasixZ/nadirniodil.github.io'
-});
+  site: 'https://github.com/FasixZ/nadirniodil.github.io'
+  })
+
 
