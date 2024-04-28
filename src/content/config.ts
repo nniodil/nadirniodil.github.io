34f715843c24b7ee785347886a8b8644b437cbbe
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-04-28 17:20:20 +0200

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ export const collections = {
 			title: z.string(),
 			description: z.string(),
 			publishDate: z.coerce.date(),
-			tags: z.array(z.string()).optional,
+			tags: z.array(),
 			img: z.string(),
 			img_alt: z.string().optional(),
 		}),
