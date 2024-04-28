9960081697b88f849a642bd5c3d25077ab0c1f8d
Nadir Niodil <165339957+FasixZ@users.noreply.github.com>
2024-04-28 14:49:17 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: "https://fasixz.github.io/",
+  site: "https://nadirniodil/",
   base: "/nadirniodil.github.io/"
 })
